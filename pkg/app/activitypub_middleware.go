b25660c2f5945fe862c8a500233809387952033b
Bastian Rihm <me@bastianjoel.de>
2026-05-10 13:30:41 +0200

    Soft fail on invalid activitypub signature

--- pkg/app/activitypub_middleware.go
+++ pkg/app/activitypub_middleware.go
@@ -1,10 +1,7 @@
 package app
 
 import (
-	"net/http"
-
 	"github.com/AepyornisNet/aepyornis/pkg/aputil"
-	"github.com/AepyornisNet/aepyornis/pkg/model/dto"
 	"github.com/AepyornisNet/aepyornis/pkg/service"
 	"github.com/labstack/echo/v4"
 	"github.com/samber/do/v2"
@@ -15,10 +12,7 @@ func (a *App) RequestingActorMiddleware(next echo.HandlerFunc) echo.HandlerFunc 
 		actor, err := do.MustInvoke[service.ActivityPubRequestService](a.injector).VerifyRequest(c.Request())
 		if err != nil {
 			a.logger.Warn("invalid ActivityPub request signature", "error", err)
-			return a.renderAPIV2Error(c, http.StatusBadRequest, dto.ErrBadRequest)
-		}
-
-		if actor != nil {
+		} else if actor != nil {
 			c.Set(aputil.RequestingActorContextKey, actor)
 		}
 
