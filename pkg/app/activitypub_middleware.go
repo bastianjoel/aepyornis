8f01a3c1dc6ed12ba89efbfae7fe38b47728aee4
Bastian Rihm <me@bastianjoel.de>
2026-05-10 15:26:21 +0200

    Readd error on invalid signature

--- pkg/app/activitypub_middleware.go
+++ pkg/app/activitypub_middleware.go
@@ -1,7 +1,10 @@
 package app
 
 import (
+	"net/http"
+
 	"github.com/AepyornisNet/aepyornis/pkg/aputil"
+	"github.com/AepyornisNet/aepyornis/pkg/model/dto"
 	"github.com/AepyornisNet/aepyornis/pkg/service"
 	"github.com/labstack/echo/v4"
 	"github.com/samber/do/v2"
@@ -12,7 +15,10 @@ func (a *App) RequestingActorMiddleware(next echo.HandlerFunc) echo.HandlerFunc 
 		actor, err := do.MustInvoke[service.ActivityPubRequestService](a.injector).VerifyRequest(c.Request())
 		if err != nil {
 			a.logger.Warn("invalid ActivityPub request signature", "error", err)
-		} else if actor != nil {
+			return a.renderAPIV2Error(c, http.StatusBadRequest, dto.ErrBadRequest)
+		}
+
+		if actor != nil {
 			c.Set(aputil.RequestingActorContextKey, actor)
 		}
 
